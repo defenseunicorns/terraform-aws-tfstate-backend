e53fd0d3ec56cc1d3a67be49f589abfb435da5a8
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2023-04-25 20:16:20 -0500

    Add comments to test

--- test/e2e/state_backend_test.go
+++ test/e2e/state_backend_test.go
@@ -19,10 +19,12 @@ func TestStateBackend(t *testing.T) {
 	defer terraform.Destroy(t, terraformOptions)
 	terraform.InitAndApply(t, terraformOptions)
 
+	// Check that the S3 bucket ID is what we expect it to be.
 	s3BucketID := terraform.Output(t, terraformOptions, "tfstate_bucket_id")
 	expectedBucketIDStartsWith := "uds-ex"
 	assert.Equal(t, true, strings.HasPrefix(s3BucketID, expectedBucketIDStartsWith), fmt.Sprintf("Expected the S3 bucket ID to start with: '%s', but got: '%s'", expectedBucketIDStartsWith, s3BucketID))
 
+	// Check that the DynamoDB table name is what we expect it to be.
 	dynamoDbTableName := terraform.Output(t, terraformOptions, "tfstate_dynamodb_table_name")
 	expectedDynamoDbTableNameStartsWith := "uds-ex-lock"
 	assert.Equal(t, true, strings.HasPrefix(dynamoDbTableName, expectedDynamoDbTableNameStartsWith), fmt.Sprintf("Expected the DynamoDB Table name to start with: '%s', but got: '%s'", expectedDynamoDbTableNameStartsWith, dynamoDbTableName))

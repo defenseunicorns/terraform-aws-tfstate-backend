0b7f86fe81b91df4e22075b7cdb5c7f35d991f7a
Will Christensen <will.christensen@defenseunicorns.com>
2023-05-01 23:50:42 -0400

    removing extra line

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 locals {
   create_bucket_policy = length(var.admin_arns) > 0 ? true : false
   backend_content = {
-
     region               = var.region
     bucket               = module.s3_bucket.s3_bucket_id
     terraform_state_file = "tfstate/${var.region}/${var.bucket_prefix}-bucket.tfstate"

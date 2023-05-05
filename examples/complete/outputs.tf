37678f4b10cfbf5b4f07d04796e8b4210ec7f35b
Bryan Finster <bryan.finster@gmail.com>
2023-05-05 09:52:59 -0500

    Test draft to ready

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -3,6 +3,7 @@ output "tfstate_bucket_id" {
   description = "Terraform State Bucket Name"
 }
 
+# TODO: remove comment
 output "tfstate_dynamodb_table_name" {
   value       = module.tfstate_backend.tfstate_dynamodb_table_name
   description = "Terraform State DynamoDB Table Name"

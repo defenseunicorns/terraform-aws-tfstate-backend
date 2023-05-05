4ca91ab40b8f94982265902c16555618b9616083
Bryan Finster <bryan.finster@gmail.com>
2023-05-05 09:36:27 -0500

    Change to test ci

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -3,6 +3,8 @@ output "tfstate_bucket_id" {
   description = "Terraform State Bucket Name"
 }
 
+#TODO: Remove this comment that's used for testing ci flows
+
 output "tfstate_dynamodb_table_name" {
   value       = module.tfstate_backend.tfstate_dynamodb_table_name
   description = "Terraform State DynamoDB Table Name"

3c5b2d44429f99054e557cc80846571607261faf
Will Christensen <will.christensen@defenseunicorns.com>
2023-05-15 19:03:11 -0400

    Adding zarf package and shell examples

---
+++ destroy-zarf.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check the processor architecture
+if [[ $(uname -m) == "x86_64" ]]; then
+  echo "The processor architecture is 64-bit"
+  ARCH_PROC=amd64
+elif [[ $(uname -m) == "i686" || $(uname -m) == "i386" ]]; then
+  echo "The processor architecture is 32-bit"
+  echo "The processor is not AMD or ARM"
+elif [[ $(uname -m) == "arm64" ]]; then
+  ARCH_PROC=arm64
+else
+# default...
+  ARCH_PROC=amd64
+fi
+
+zarf package deploy zarf-package-terraform-aws-tfstate-backend-${ARCH_PROC}.tar.zst \
+	--set region="us-east-2" \
+	--set bucket_prefix="exampletfstate-" \
+	--set versioning_enabled="false" \
+	--set force_destroy="true" \
+	--components backend-destroy \
+	--confirm
+

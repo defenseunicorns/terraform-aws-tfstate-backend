e58e33373c70b8979154995f4b541b3f1cc37dc7
zamaz <71521611+zachariahmiller@users.noreply.github.com>
2023-06-12 17:05:42 -0400

    remove unused locals

--- locals.tf
+++ locals.tf
@@ -1,4 +1,9 @@
 locals {
-  role_name     = "${var.bucket_prefix}-role"
-  bucket_prefix = var.bucket_prefix
+  create_bucket_policy = length(var.admin_arns) > 0 ? true : false
+  backend_content = {
+    region               = var.region
+    bucket               = module.s3_bucket.s3_bucket_id
+    terraform_state_file = "tfstate/${var.region}/${module.s3_bucket.s3_bucket_id}-bucket.tfstate"
+    dynamodb_table       = aws_dynamodb_table.dynamodb_terraform_state_lock.id
+  }
 }
